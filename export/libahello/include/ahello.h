c06550530921fce0c4191b9c6f24a9429ea6bca7
xong <lilinxiong1997@gmail.com>
2018-09-26 11:47:09 +0800

    again add export dir

---
+++ export/libahello/include/ahello.h
@@ -0,0 +1,10 @@
+//
+// Created by xong on 2018/9/25.
+//
+
+#ifndef USECMAKEBUILDLIB_LIBAHELLO_H
+#define USECMAKEBUILDLIB_LIBAHELLO_H
+
+int ahello(int i);
+
+#endif //USECMAKEBUILDLIB_LIBAHELLO_H
---
+++ export/libsohello/include/sohello.h
@@ -0,0 +1,10 @@
+//
+// Created by xong on 2018/9/25.
+//
+
+#ifndef USECMAKEBUILDLIB_SOHELLO_H
+#define USECMAKEBUILDLIB_SOHELLO_H
+
+int sohello(int i );
+
+#endif //USECMAKEBUILDLIB_SOHELLO_H
